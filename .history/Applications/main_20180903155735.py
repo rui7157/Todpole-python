98d765e29f4ccfbba1d03f3e8a64be6f156bba42
nvray <769007157@qq.com>
2018-10-21 22:08:14 +0800

    删除配置文件

--- .history/Applications/main_20180903155735.py
+++
@@ -1,1 +0,0 @@
-from todpole import events
--- .history/Applications/main_20180903155757.py
+++
@@ -1,6 +0,0 @@
-from todpole import events
-
-
-print("Running on http://127.0.0.1:8282/ (Press CTRL+C to quit)")
-events.WSGIServer(("127.0.0.1", 8282), app,
-            handler_class=WebSocketHandler).serve_forever()
